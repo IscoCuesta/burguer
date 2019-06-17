1053c38ebfbe48ee5956a0f60117f380fec4ac85
Isco <Iscocuesta@hotmail.com>
2019-06-17 12:14:24 -0500

    package json and app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/users');
@@ -13,11 +11,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 var PORT = process.env.PORT || 8080;
 
 app.use('/', indexRouter);
