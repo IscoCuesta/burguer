c21249a19cbaba9e6b91591ceae363b24bdbf8eb
Isco <Iscocuesta@hotmail.com>
2019-06-18 11:02:12 -0500

    final changes

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,12 @@ create database burguers_db;
 
 use burguers_db;
 
+DROP TABLE burguers;
 CREATE TABLE burguers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(70) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
+    createdAt timestamp NOT NULL,
 	PRIMARY KEY (id)
 );
